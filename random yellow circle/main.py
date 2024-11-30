e7a3c309f396d4c9137b367831a0ee9f21d1b5c9
badnumberr <ymniychel000@gmail.com>
2024-11-30 17:26:08 +0500

    yellow_circles

---
+++ random yellow circle/main.py
@@ -0,0 +1,34 @@
+import sys
+import random
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('круги')
+        self.circles = []
+        self.pushButton.clicked.connect(self.add_circle)
+
+    def add_circle(self):
+        d = random.randint(20, 100)
+        x = random.randint(0, self.width() - d)
+        y = random.randint(0, self.height() - d)
+        self.circles.append((x, y, d))
+        self.update()
+
+    def paintEvent(self, event):
+        paint = QPainter(self)
+        paint.setBrush(QColor('yellow'))
+        for x, y, d in self.circles:
+            paint.drawEllipse(x, y, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
